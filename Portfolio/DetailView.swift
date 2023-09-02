cc760431e58324605b97c6fb58385fe0d6b3f580
Emma Walker <walkere@tcd.ie>
2023-09-02 13:14:32 +0100

    connects core data to swiftUI

---
+++ Portfolio/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  Portfolio
+//
+//  Created by Emma Walker - TVandMobile Platforms - Core Engineering on 02/09/2023.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Detail")
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
---
+++ Portfolio/SidebarView.swift
@@ -0,0 +1,20 @@
+//
+//  SidebarView.swift
+//  Portfolio
+//
+//  Created by Emma Walker - TVandMobile Platforms - Core Engineering on 02/09/2023.
+//
+
+import SwiftUI
+
+struct SidebarView: View {
+    var body: some View {
+        Text("Sidebar")
+    }
+}
+
+struct SidebarView_Previews: PreviewProvider {
+    static var previews: some View {
+        SidebarView()
+    }
+}
