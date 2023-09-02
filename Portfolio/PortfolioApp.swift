f4e1269fce9a1a563945cb523a4f9827a2829227
Emma Walker <walkere@tcd.ie>
2023-09-02 11:36:36 +0100

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Emma Walker - TVandMobile Platforms - Core Engineering on 02/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
